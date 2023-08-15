27f302593a167935f6150f11f4abda271446084d
SomikSom <141712025+SomikSom@users.noreply.github.com>
2023-08-15 14:30:05 +0300

    4.1 четвертый комит

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,8 @@
 from selenium.webdriver.common.by import By
+
+from pages.locators import MainPageLocators
 from .pages.main_page import MainPage
+
 
 link = "http://selenium1py.pythonanywhere.com/"
 
@@ -11,12 +14,11 @@ def go_to_login_page(browser):
 
 
 def test_guest_can_go_to_login_page(browser):
-   browser.get(link)
-   go_to_login_page(browser)
+    browser.get(link)
+    go_to_login_page(browser)
 
 
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()      # выполняем метод страницы — переходим на страницу логина
